bec56ff72d257b2be631b5ab8d470fcedd9858e2
vdang <dbacvan@gmail.com>
2022-10-30 21:31:02 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -59,49 +59,49 @@ namespace robotic_arm_extension {
         basic.pause(200)
         put_claw_in_default_position()
     }
-    //% block="open claw"
+    //% block="open|claw %angle_step"
     //% angle_step.defl=10
     export function open_claw(angle_step: number = 10) {
         claw_angle = Math.max(claw_angle - angle_step, claw_angle_min)
         pins.servoWritePin(AnalogPin.P8, claw_angle)
     }
-    //% block
+    //% block="close|claw %angle_step"
     //% angle_step.defl=10
     export function close_claw(angle_step: number = 10) {
         claw_angle = Math.min(claw_angle + angle_step, claw_angle_max)
         pins.servoWritePin(AnalogPin.P8, claw_angle)
     }
-    //% block
+    //% block="rotate|base|left %angle_step"
     //% angle_step.defl=3
     export function rotate_base_left(angle_step: number = 3) {
         base_angle = Math.min(base_angle + angle_step, base_angle_max)
         pins.servoWritePin(AnalogPin.P9, base_angle)
     }
-    //% block
+    //% block="rotate|base|right %angle_step"
     //% angle_step.defl=3
     export function rotate_base_right(angle_step: number = 3) {
         base_angle = Math.max(base_angle - angle_step, base_angle_min)
         pins.servoWritePin(AnalogPin.P9, base_angle)
     }
-    //% block
+    //% block="lift|top|arm %angle_step"
     //% angle_step.defl=3
     export function lift_top_arm (angle_step: number = 3) {
         top_angle = Math.max(top_angle - angle_step, top_angle_min)
         pins.servoWritePin(AnalogPin.P1, top_angle)
     }
-    //% block
+    //% block="lower|top|arm %angle_step"
     //% angle_step.defl=3
     export function lower_top_arm(angle_step: number = 3) {
         top_angle = Math.min(top_angle + angle_step, top_angle_max)
         pins.servoWritePin(AnalogPin.P1, top_angle)
     }
-    //% block
+    //% block="lift|middle|arm %angle_step"
     //% angle_step.defl=3
     export function lift_middle_arm(angle_step: number = 3) {
         mid_angle = Math.min(mid_angle + angle_step, mid_angle_max)
         pins.servoWritePin(AnalogPin.P2, mid_angle)
     }
-    //% block
+    //% block="lower|middle|arm2 %angle_step"
     //% angle_step.defl=3
     export function lower_middle_arm(angle_step: number = 3) {
         mid_angle = Math.max(mid_angle - angle_step, mid_angle_min)
